7cb80dc22e8d9df68ec22e9e78640812168e6e4b
liu <2100787301@qq.com>
2022-10-17 17:00:47 +0800

    backup

--- new.bat
+++ new.bat
@@ -2,6 +2,6 @@ echo f | xcopy mode.md  New.md
 set /p a=请输入文件名:
 
 ren New.md  %a%.md
-
-move %a%.md D:\blog\liushuhuang.github.io\source\_posts
-start D:\blog\liushuhuang.github.io\source\_posts\%a%.md
+set curdir=%~dp0
+move %a%.md  %curdir%\source\_posts
+start  %curdir%\source\_posts\%a%.md
